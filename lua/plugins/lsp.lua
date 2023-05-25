dc8bf185e57d5fdc770a6e5359d8bbe89e0796ea
itsmariush <itsmariush@users.noreply.github.com>
2023-05-26 00:08:33 +0200

    configured rust-tools

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,37 @@
+--- nice config https://github.com/appelgriebsch/Nv/blob/main/lua/plugins/extras/lang/rust.lua
 return {
+  -- extend auto completion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "Saecki/crates.nvim",
+        event = { "BufRead Cargo.toml" },
+        config = true,
+      },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = "crates" },
+      }))
+    end,
+  },
+  -- add rust to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "ron", "rust", "toml" })
+    end,
+  },
+  -- correctly setup mason lsp extensions
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "rust-analyzer", "taplo" })
+    end,
+  },
   {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -10,7 +43,38 @@ return {
       },
       setup = {
         rust_analyzer = function(_, opts)
-          require("rust-tools").setup({ server = opts })
+          local mason_registry = require("mason-registry")
+          local rust_tools_opts = {
+            tools = {
+              hover_actions = {
+                auto_focus = false,
+                border = "none",
+              },
+              inlay_hints = {
+                auto = true,
+                show_parameter_hints = true,
+              },
+            },
+            server = vim.tbl_deep_extend("force", opts, {
+              settings = {
+                ["rust-analyzer"] = {
+                  cargo = {
+                    features = "all",
+                  },
+                  -- Add clippy lints for Rust.
+                  checkOnSave = true,
+                  check = {
+                    command = "clippy",
+                    features = "all",
+                  },
+                  procMacro = {
+                    enable = true,
+                  },
+                },
+              },
+            }),
+          }
+          require("rust-tools").setup(rust_tools_opts)
           return true
         end,
       },
